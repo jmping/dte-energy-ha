c269fa79b2e81183ae28c393697b7bb59caf5cec
Josh Pasek <josh@joshpasek.com>
2026-09-19 18:31:31 -0400

    Add combined DTE service type

--- custom_components/dte_energy/const.py
+++ custom_components/dte_energy/const.py
@@ -2,25 +2,18 @@
 
 DOMAIN = "dte_energy"
 
-# Configuration keys
 CONF_USAGE_LINK = "usage_link"
 CONF_SERVICE_TYPE = "service_type"
 
-# Service types
 SERVICE_TYPE_ELECTRIC = "electric"
 SERVICE_TYPE_GAS = "gas"
+SERVICE_TYPE_COMBINED = "combined"
 
-# Update interval (in hours)
 DEFAULT_UPDATE_INTERVAL = 24
 
-# Green Button XML namespaces
 NAMESPACES = {
     "atom": "http://www.w3.org/2005/Atom",
     "espi": "http://naesb.org/espi",
 }
 
-# Unit conversions
-# CCF to cubic feet (1 CCF = 100 cubic feet)
 CCF_TO_CUBIC_FEET = 100
-# Therms to cubic feet (approximate, depends on gas heat content)
-THERMS_TO_CUBIC_FEET = 100
